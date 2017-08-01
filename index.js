8401d8ee0f4e092e5261aca04f4ddd5d691319aa
Luana Martins <lms7@cin.ufpe.br>
2017-08-01 06:50:36 -0300

    Add file indexjs of initial tests

---
+++ index.js
@@ -0,0 +1,23 @@
+var esprima = require('esprima');
+var acorn = require('acorn');
+var fs = require('fs');
+
+var filepath = '';
+
+//var program = 'const answer = 42';
+
+// var tokens = esprima.tokenize(program);
+// console.log(tokens);
+// [ { type: 'Keyword', value: 'const' },
+//   { type: 'Identifier', value: 'answer' },
+//   { type: 'Punctuator', value: '=' },
+//   { type: 'Numeric', value: '42' } ]
+
+// var programParsed = esprima.parse(program);
+// console.log(programParsed);
+// { type: 'Program',
+//   body:
+//    [ { type: 'VariableDeclaration',
+//        declarations: [Object],
+//        kind: 'const' } ],
+//   sourceType: 'script' }
