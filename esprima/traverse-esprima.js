0d6e3250591deed99532f744217aed1351419e33
Luana Martins <lms7@cin.ufpe.br>
2017-08-03 06:51:24 -0300

    Get ast from a file

--- esprima/traverse-esprima.js
+++ esprima/traverse-esprima.js
@@ -2,14 +2,14 @@ var fs = require('fs');
 var esprima = require('esprima');
 var estraverse = require('estraverse');
 
-var filename = process.argv[2];
-console.log('Processing', filename);
+var filename = 'output.txt';
+//var filename = process.argv[2];
 //fs.readFileSync(filename)
-var ast = esprima.parse('var answer = 42;');
-console.log(ast);
+var ast = esprima.parse(fs.readFileSync(filename));
+
 estraverse.traverse(ast, {
     enter: function(node){
-        if (node.type === 'AssignmentExpression'){
+        if (node.type === 'CatchClause'){
             console.log('Encountered assignment to', node.left.name);
         }
     }
