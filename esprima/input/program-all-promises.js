4df61cb19498184d9d6e10d98140976500d70089
Luana Martins <lms7@cin.ufpe.br>
2017-08-03 07:19:18 -0300

    Add setTimeout on p3

--- esprima/input/program-all-promises.js
+++ esprima/input/program-all-promises.js
@@ -1,13 +1,11 @@
 var p1 = Promise.resolve(10);
 var p2 = Promise.resolve(19);
-var p3 = Promise.reject('Ops!');
+var p3 = new Promise(function(resolve, reject){
+   setTimeout(function(){
+       Promise.reject('Ops!');
+   }, 1000);
+});
 
 Promise.all([p1, p2, p3]).then((values) =>{
     console.log(values);
 });
-
-/*
-* (node:15550) UnhandledPromiseRejectionWarning: Unhandled promise rejection (rejection id: 2): Ops!
- (node:15550) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-
- * */
