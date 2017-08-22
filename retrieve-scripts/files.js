79eb507f050834adb6e482af077f28ba737a0bd6
Luana Martins <lms7@cin.ufpe.br>
2017-08-22 01:03:22 -0300

    Add method to write csv files

--- retrieve-scripts/files.js
+++ retrieve-scripts/files.js
@@ -11,11 +11,34 @@ function copyFile(src, dest) {
     readStream.pipe(fs.createWriteStream(dest));
 }
 
-function readFileSync(filepath){
+function readFileSync(filepath) {
     return fs.readFileSync(filepath, 'utf8');
+}
+
+function writeCsvFile(filename, fields, data) {
+    var json2csv = require('json2csv');
+    const extension = '.csv';
+    // var fields = ['field1', 'field2', 'field3'];
+    //
+    // var myData = [
+    //     {
+    //         "field1": "t",
+    //         "field2": "t",
+    //         "field3": "t"
+    //     }
+    // ];
+
+    var filepath = path.join(filename, extension);
+    var csv = json2csv({ data: data, fields: fields });
+    fs.writeFile(filepath, csv, function(err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
+
 }
 
 module.exports = {
     copyFile: copyFile,
-    readFileSync: readFileSync
+    readFileSync: readFileSync,
+    writeCsvFile: writeCsvFile
 }
