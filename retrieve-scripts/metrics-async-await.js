d3f19fccae7a4405083254ef8d2672b36ae9150a
Luana Martins <lms7@cin.ufpe.br>
2017-08-24 06:41:08 -0300

    Add considerations as comment

--- retrieve-scripts/metrics-async-await.js
+++ retrieve-scripts/metrics-async-await.js
@@ -1,4 +1,6 @@
 function handleAnalysis(obj, repoObject) {
+
+    // Check if it is possible to create async anonymous functions
     if(obj.type == 'FunctionDeclaration' && obj.async){
         repoObject.numberOfAsyncs++;
     }
