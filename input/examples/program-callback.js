1996caf9fb64a9b6ad2f9084609900f28067bd03
Luana Martins <lms7@cin.ufpe.br>
2017-08-24 06:48:53 -0300

    Update callback examples

--- input/examples/program-callback.js
+++ input/examples/program-callback.js
@@ -1,4 +1,10 @@
-readFile(function (err, data) {
+function printFile(callback){
+    callback('Oops', ['a', 'b', 'c']);
+}
+
+printFile(function (err, data) {
     if (err) return console.error(err);
     console.log(data);
 });
+
+printFile(console.log);
