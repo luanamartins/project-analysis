9cfa86d86535bbe9c0002ae59a5dae04ee6abbba
Luana Martins <lms7@cin.ufpe.br>
2017-09-22 07:24:53 -0300

    Retrieve data from callback function which has any keywords of interest

--- retrieve-scripts/metrics-callback.js
+++ retrieve-scripts/metrics-callback.js
@@ -1,6 +1,6 @@
 function handleAnalysis(obj, repoObject) {
 
-    if(obj.type == 'FunctionDeclaration'){
+    if (obj.type == 'FunctionDeclaration') {
         // If a function argument is called inside the function, this function is callback-accepting
 
         // Let's use heuristics to identify callbacks for error handling:
@@ -10,10 +10,10 @@ function handleAnalysis(obj, repoObject) {
         // Still needs to consider ArrowExpressions () => {} (Not exactly)
 
         var functionsArgs = obj.params;
-        if(obj.body){
+        if (obj.body) {
             var functionBody = obj.body.body;
 
-            if(functionBody) {
+            if (functionBody) {
                 functionBody.forEach(function (statement) {
                     if (statement.type == 'CallExpression') {
                         const calleeName = statement.callee.name;
@@ -26,7 +26,34 @@ function handleAnalysis(obj, repoObject) {
         }
     }
 
+    if (obj.type == 'ExpressionStatement' && obj.expression == 'CallExpression') {
+        var callArgs = obj.expression.arguments;
+        var callbacks = callArgs.filter(filterCallbacks);
+    }
+
 }
+
+// Functions which has no arguments, but still uses:
+//  console.error
+//  console.log a message of error
+//  Still are handling errors
+
+function filterCallbacks(arg) {
+    if (arg.type == 'FunctionExpression') {
+        var params = arg.params;
+        var keywords = ['err', 'error', 'e', 'exception'];
+
+        if (params && findOne(keywords, params)) {
+
+        }
+    }
+}
+
+var findOne = function (haystack, arr) {
+    return arr.some(function (v) {
+        return haystack.indexOf(v.name) >= 0;
+    });
+};
 
 module.exports = {
     handleAnalysis: handleAnalysis
