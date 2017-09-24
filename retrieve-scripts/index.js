dfa20975a0f0e242198683daf217f281373f6823
Luana Martins <lms7@cin.ufpe.br>
2017-09-24 16:18:59 -0300

    Handle checking out repositories

--- retrieve-scripts/index.js
+++ retrieve-scripts/index.js
@@ -6,19 +6,29 @@ const repoModule = require('./repository.js');
 const metricsModule = require('./metrics.js');
 
 var projectPath = process.env.RETRIEVE_SCRIPTS_ROOT_PATH;
-
 console.log(projectPath);
-var inputGithubFilepath = path.join(projectPath, 'github.txt');
+
+let inputGithubFilepath = path.join(projectPath, 'github.txt');
 var outputGithubFilepath = path.join(projectPath, 'repos');
+const checkoutRepos = false;
 
 function main() {
-    var repos = repoModule.getRepos(inputGithubFilepath);
-    repos.forEach(function (repo) {
-        repoModule.checkoutRepoTo(repo, outputGithubFilepath);
-        var repoName = repoModule.getRepoProjectName(repo);
-        var repoOutputDir = path.join(outputGithubFilepath, repoName);
-        var files = repoModule.getFilesFromDir(repoOutputDir, ['.js'], ['.min.js']);
-        metricsModule.handleMetrics(repoOutputDir, files);
+    let repositoriesName = [];
+    if (checkoutRepos) {
+        let reposToCheckout = repoModule.getRepos(inputGithubFilepath);
+        reposToCheckout.forEach(function (repo) {
+            repoModule.checkoutRepoTo(repo, outputGithubFilepath);
+            let repoName = repoModule.getRepoProjectName(repo);
+            repositoriesName.push(repoName);
+        });
+    } else {
+        repositoriesName = ['bootstrap'];
+    }
+
+    repositoriesName.forEach(function (repositoryName) {
+        let repoOutputDirectory = path.join(outputGithubFilepath, repositoryName);
+        let files = repoModule.getFilesFromDir(repoOutputDirectory, ['.js'], ['.min.js']);
+        metricsModule.handleMetrics(repoOutputDirectory, files);
         //console.log(files);
     });
 }
