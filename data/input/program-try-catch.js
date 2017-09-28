0ab50e5401304143b6d32dacf1d8e98bd0911728
Luana Martins <lms7@cin.ufpe.br>
2017-09-28 00:17:05 -0300

    Add example with different types of error raised

--- data/input/program-try-catch.js
+++ data/input/program-try-catch.js
@@ -1,6 +1,9 @@
 try {
     callHook();
-    //throw new Error('Ops');
+    throw new Error('Ops');
+    throw 32;
+    throw 'Testing throw';
+    throw true;
 } catch (error) {
     test.pushFailure(hookName + " failed on " + test.testName + ": " + (error.message || error), extractStacktrace(error, 0));
 }
