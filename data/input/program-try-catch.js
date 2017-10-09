8e1f3462c21a9ea9f974b7ef3b91b637768b2052
Luana Martins <lms7@cin.ufpe.br>
2017-10-09 07:10:32 -0300

    Move try-catch to a function

--- data/input/program-try-catch.js
+++ data/input/program-try-catch.js
@@ -1,9 +1,12 @@
-try {
-    callHook();
-    throw new Error('Ops');
-    throw 32;
-    throw 'Testing throw';
-    throw true;
-} catch (error) {
-    console.log('Error');
+
+function calling() {
+    try {
+        callHook();
+        throw new Error('Ops');
+        throw 32;
+        throw 'Testing throw';
+        throw true;
+    } catch (error) {
+        console.log('Error');
+    }
 }
