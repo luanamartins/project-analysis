563092f6c60af45c12544c6ff001d1a2e8d1c5cb
Luana Martins <lms7@cin.ufpe.br>
2017-10-14 10:48:08 -0300

    Format code

--- data/input/program-all-promises.js
+++ data/input/program-all-promises.js
@@ -1,11 +1,11 @@
 var p1 = Promise.resolve(10);
 var p2 = Promise.resolve(19);
-var p3 = new Promise(function(resolve, reject){
-   setTimeout(function(){
-       Promise.reject('Ops!');
-   }, 1000);
+var p3 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        Promise.reject('Ops!');
+    }, 1000);
 });
 
-Promise.all([p1, p2, p3]).then((values) =>{
+Promise.all([p1, p2, p3]).then((values) => {
     console.log(values);
 });
