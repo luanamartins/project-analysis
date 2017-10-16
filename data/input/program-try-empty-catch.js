d343facab0b8fbaa9bf4cc87a6f7d0ec40224c6e
Luana Martins <lms7@cin.ufpe.br>
2017-10-15 23:33:37 -0300

    Add for statement on data examples

--- data/input/program-try-empty-catch.js
+++ data/input/program-try-empty-catch.js
@@ -3,6 +3,10 @@ function calling() {
     try {
     } catch (error) {
         console.log('Error');
+        for (i = 0; i < 100; i++)
+        {
+            console.log('hello');
+        }
     } finally {
         console.log('');
         console.log('');
