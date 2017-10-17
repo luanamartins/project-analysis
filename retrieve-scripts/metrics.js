1f41fa0ce5a5fe445f8b8d85e8ef87b111dbafa9
Luana Martins <lms7@cin.ufpe.br>
2017-10-17 00:04:20 -0300

    Move reportObject to external file

--- retrieve-scripts/metrics.js
+++ retrieve-scripts/metrics.js
@@ -1,5 +1,6 @@
 const esprima = require('esprima');
 const path = require('path');
+var jsonfile = require('jsonfile');
 
 const fileModule = require('./files.js');
 const tryCatchModule = require('./metrics-try-catch.js');
@@ -21,87 +22,14 @@ function traverse(obj, fn) {
     }
 }
 
-let repoObject = {
-    totalOfJSFiles: 0,
-    totalOfJSLines: 0,
-    totalOfJSFilesErrorHandler: 0, // TODO
+function handleMetrics(files, jsonFilepath) {
 
-    tryCatch: {
-        numberOfTries: 0,
-        numberOfEmptyTries: 0, // TODO test
-        numberOfTriesLines: 0, // TODO test
-        numberOfTriesWithUniqueStatement: 0, // TODO test
-
-        numberOfCatches: 0,
-        numberOfEmptyCatches: 0,
-        numberOfCatchesLines: 0, // TODO test
-        numberOfUniqueConsole: 0,
-        numberOfCatchesWithUniqueStatement: 0, // TODO test
-
-        numberOfThrows: 0,
-        numberOfThrowsLiteral: 0,
-        numberOfThrowsErrorObjects: 0,
-
-        numberOfFinallies: 0, // TODO
-        numberOfFinalliesLines: 0, // TODO
-    },
-
-    promise: {
-        numberOfPromises: 0,
-        numberOfResolves: 0,
-        numberOfRejects: 0,
-
-        numberOfPromiseThens: 0,
-        numberOfPromiseThenLines: 0, // TODO
-        numberOfPromiseCatches: 0,
-        numberOfPromiseCatchesLines: 0, // TODO
-        numberOfEmptyFunctionsOnPromiseCatches: 0, // TODO
-
-        numberOfPromiseRaces: 0,
-        numberOfPromiseAll: 0,
-    },
-
-    asyncAwait: {
-        numberOfAsyncs: 0,
-        numberOfAwaits: 0,
-
-        numberOfTries: 0, // TODO
-        numberOfTriesLines: 0, // TODO
-
-        numberOfCatches: 0,
-        numberOfEmptyCatches: 0,
-        numberOfCatchesLines: 0,
-        numberOfUniqueConsole: 0,
-
-        numberOfFinallies: 0, // TODO
-        numberOfFinalliesLines: 0, // TODO
-    },
-
-
-    events: {
-        numberOfEventMethodsOn: 0,
-        numberOfEventMethodsOnce: 0,
-        numberOfEventMethodsEmit: 0,
-
-        numberOfEventUncaughtException: 0,
-        numberOfEventUnhandledRejection: 0,
-
-        totalOfStringEvents: 0,
-        totalOfEventTypes: 0
-    },
-
-    callbacks: {
-        numberOfCallbackAcceptingFunctions: 0,
-        numberOfCallbackErrorFunctions: 0,
-        numberOfEmptyCallbacks: 0
-    }
-};
-
-function handleMetrics(files) {
-
+    let metrics = [];
+    //console.log('Files: ', files);
     if (files) {
         files.forEach(function (fullFilepath) {
             try {
+                let repoObject = jsonfile.readFileSync(jsonFilepath);
                 repoObject.totalOfJSLines += files.length;
                 repoObject.totalOfJSLines += fileModule.countLinesOnFile(fullFilepath);
 
@@ -116,20 +44,19 @@ function handleMetrics(files) {
                 // checkEventsTypes(ast, eventEmitterObject);
                 getMetrics(ast, fullFilepath, repoObject);
                 // getJSFilesEHM(ast, repoObject);
+                metrics.push(repoObject);
             } catch (err) {
-                console.log(fullFilepath);
+                console.log(err);
             }
         });
     }
-
-    return repoObject;
+    //console.log('Metrics ', metrics);
+    return metrics;
 }
 
 function getMetrics(ast, filepath, reportObject) {
 
     try {
-        console.log(filepath);
-
         traverse(ast, function (node) {
             tryCatchModule.handleAnalysis(node, reportObject);
             promiseModule.handleAnalysis(node, reportObject);
