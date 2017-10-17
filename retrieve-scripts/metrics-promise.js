4a7857f4db1fd7532f6f24680df8167327bdf643
Luana Martins <lms7@cin.ufpe.br>
2017-10-17 00:03:40 -0300

    Fix an error related to number of resolves on promises

--- retrieve-scripts/metrics-promise.js
+++ retrieve-scripts/metrics-promise.js
@@ -7,23 +7,19 @@ function handleAnalysis(node, reportObject) {
     if (node.type === 'CallExpression') {
         const callee = node.callee;
         if (callee) {
-            if (callee.name === 'resolve') {
-                reportObject.promise.numberOfResolves++;
-            }
 
-            if (callee.name === 'reject') {
-                reportObject.promise.numberOfRejects++;
-            }
 
             if (callee.property) {
+                if (callee.property.name === 'resolve') {
+                    reportObject.promise.numberOfResolves++;
+                }
+
+                if (callee.property.name === 'reject') {
+                    reportObject.promise.numberOfRejects++;
+                }
+
                 if (callee.property.name === 'then') {
                     reportObject.promise.numberOfPromiseThens++;
-
-                    // const callbackFunction = node.arguments[0];
-                    // const callbackFunctionBodyLoc = callbackFunction.body.loc;
-                    // const start = callbackFunctionBodyLoc.start.line;
-                    // const end = callbackFunctionBodyLoc.end.line;
-                    // reportObject.promise.numberOfPromiseThenLines += (end - start);
                 }
 
                 if (callee.property.name === 'catch') {
