98a37f436f0512a8ed1c6eae5d02258289b438a8
Luana Martins <lms7@cin.ufpe.br>
2017-10-19 07:02:27 -0300

    Experimenting the library for mann-whitney u test

--- statistics/analysis.py
+++ statistics/analysis.py
@@ -1,7 +1,15 @@
-from scipy.stats import mannwhitneyu
+# from scipy.stats import mannwhitneyu
 from numpy import genfromtxt
+import numpy as np
+from scipy import stats
 
 bootstrap_data = genfromtxt('data/bootstrap.csv', delimiter=',', skip_header=1)
 socket_io_data = genfromtxt('data/socket.io.csv', delimiter=',', skip_header=1)
 
-# mannwhitneyu(a, b)
+#result = mannwhitneyu(bootstrap_data, socket_io_data)
+#print(result)
+
+a = np.arange(25)
+print(a)
+b = np.arange(25)+4
+print(stats.mannwhitneyu(a , b, alternative='two-sided'))
