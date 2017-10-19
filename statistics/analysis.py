cbbf70523d9df05f5bb010ea0fd94fe6fed462de
Luana Martins <lms7@cin.ufpe.br>
2017-10-19 07:24:05 -0300

    Code formatting

--- statistics/analysis.py
+++ statistics/analysis.py
@@ -6,10 +6,10 @@ from scipy import stats
 bootstrap_data = genfromtxt('data/bootstrap.csv', delimiter=',', skip_header=1)
 socket_io_data = genfromtxt('data/socket.io.csv', delimiter=',', skip_header=1)
 
-#result = mannwhitneyu(bootstrap_data, socket_io_data)
-#print(result)
+# result = mannwhitneyu(bootstrap_data, socket_io_data)
+# print(result)
 
 a = np.arange(25)
 print(a)
-b = np.arange(25)+4
-print(stats.mannwhitneyu(a , b, alternative='two-sided'))
+b = np.arange(25) + 4
+print(stats.mannwhitneyu(a, b, alternative='two-sided'))
