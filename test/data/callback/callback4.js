d5e9b8868e7cd3e6c9b6464b9af1559a8859082a
Luana Martins <lms7@cin.ufpe.br>
2017-10-22 17:32:11 -0300

    Add test files for callback functions

---
+++ test/data/callback/callback4.js
@@ -0,0 +1,14 @@
+function onError(error) {
+    console.log("Error: " + error);
+}
+
+downloadAsync("a.txt", function(error, a) {
+    if (error) return onError(error);
+    downloadAsync("b.txt", function(error, b) {
+        if (error) return onError(error);
+        downloadAsync(url3, function(error, c) {
+            if (error) return onError(error);
+            console.log("Contents: " + a + b + c);
+        });
+    });
+});
