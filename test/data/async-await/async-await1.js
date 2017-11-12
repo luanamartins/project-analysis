238aef166fe092084eff1f21b14b2d1825bd28ac
Luana Martins <lms7@cin.ufpe.br>
2017-11-12 11:02:07 -0300

    Change require on async-await test

--- test/data/async-await/async-await1.js
+++ test/data/async-await/async-await1.js
@@ -1,4 +1,4 @@
-const superagent = require('superagent');
+const http = require('http');
 
 const NUM_RETRIES = 3;
 
@@ -8,7 +8,7 @@ async function test() {
     let i;
     for (i = 0; i < NUM_RETRIES; ++i) {
         try {
-            await superagent.get('http://google.com/this-throws-an-error');
+            await http.get('http://google.com/this-throws-an-error');
             break;
         } catch(err) {}
     }
