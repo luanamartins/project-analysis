abe57ab555adc97913925d23d83f79b11cbc91b3
Luana Martins <lms7@cin.ufpe.br>
2017-11-12 10:45:40 -0300

    Add events unit tests

--- retrieve-scripts/metrics-event.js
+++ retrieve-scripts/metrics-event.js
@@ -1,54 +1,81 @@
 function handleAnalysis(node, reportObject) {
 
+    const keywords = ['err', 'error', 'exception', 'reason', 'er'];
+
     let eventListeningMethods = ['on', 'once'];
     let eventRaisingMethods = ['emit'];
-    
+
     if (node.type === 'CallExpression') {
-        if (node.property) {
-            const methodName = node.property.name;
-
-            if (methodName === 'on') {
-                reportObject.events.numberOfEventMethodsOn++;
-            }
-
-            if (methodName === 'emit') {
-                reportObject.events.numberOfEventMethodsEmit++;
-            }
-
-            if (methodName === 'once') {
-                reportObject.events.numberOfEventMethodsOnce++;
-            }
-            const firstArg = methodName.argument[0];
-
-            if (firstArg.value === 'uncaughtException') {
-                reportObject.events.numberOfEventUncaughtException++;
-            }
-
-            if (firstArg.value === 'unhandledRejection') {
-                reportObject.events.numberOfEventUnhandledRejection++;
-            }
-
-        }
-
+        // if (node.property) {
+        //     const methodName = node.property.name;
+        //
+        //     if (methodName === 'on') {
+        //         reportObject.events.numberOfEventMethodsOn++;
+        //     }
+        //
+        //     if (methodName === 'emit') {
+        //         reportObject.events.numberOfEventMethodsEmit++;
+        //     }
+        //
+        //     if (methodName === 'once') {
+        //         reportObject.events.numberOfEventMethodsOnce++;
+        //     }
+        // }
 
         if (node.callee.property) {
+
             let methodName = node.callee.property.name;
+            const firstArg = node.arguments[0];
+            const firstArgEhm = containsSubstring(keywords, firstArg.value);
+
+            if (eventListeningMethods.includes(methodName)) {
+                if (methodName === 'on' && firstArgEhm) {
+                    reportObject.events.numberOfEventMethodsOn++;
+                } else if (methodName === 'once' && firstArgEhm) {
+                    reportObject.events.numberOfEventMethodsOnce++;
+                }
+            }
+
+            if (eventRaisingMethods.includes(methodName)) {
+                if (methodName === 'emit' && firstArgEhm) {
+                    reportObject.events.numberOfEventMethodsEmit++;
+                }
+            }
+
             if (eventListeningMethods.includes(methodName) || eventRaisingMethods.includes(methodName)) {
-                const firstArgFromMethod = node.arguments[0];
-                if (firstArgFromMethod.type === 'Literal') {
+                if (firstArg.type === 'Literal') {
                     let literalValue = node.arguments[0].raw;
 
                     // the method is an event listener or emitter and is listing/raising a string as event
                     if (literalValue.startsWith("'") && literalValue.endsWith("'")) {
                         reportObject.events.totalOfStringEvents++;
                     }
+
+                    if (firstArg && firstArg.value === 'uncaughtException') {
+                        reportObject.events.numberOfEventUncaughtException++;
+                    }
+
+                    if (firstArg && firstArg.value === 'unhandledRejection') {
+                        reportObject.events.numberOfEventUnhandledRejection++;
+                    }
+                } else {
+                    reportObject.events.totalOfEventTypes++;
                 }
-                reportObject.events.totalOfEventTypes++;
             }
         }
 
     }
 }
+
+function containsSubstring(array, item) {
+    const contains = array.some(function (arrayItem) {
+        return item.indexOf(arrayItem) >= 0;
+    });
+
+    return contains;
+}
+
+// console.log(containsSubstring(['err', 'error', 'exception', 'reason', 'er'], 'connection'));
 
 module.exports = {
     handleAnalysis: handleAnalysis
