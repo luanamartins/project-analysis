9e90f8903d797f7c0d86900b1b94e895d0864170
Luana Martins <lms7@cin.ufpe.br>
2017-11-17 07:40:11 -0300

    Rename analysis.py to index.py

--- statistics/src/index.py
+++ statistics/src/index.py
@@ -27,40 +27,79 @@ def main():
     objects_client = ('Bootstrap', 'reveal.js', 'three.js', 'brackets', 'pdf.js', 'PhotoSwipe')
     objects_server = ('Express', 'Hexo', 'Node', 'Node-inspector', 'parse-server', 'socket.io')
 
-    titles = ['Number of error handling callback functions', 'Number of Error-first callback functions',
-              'Number of empty error handling callbacks', 'Number of logging error handling callbacks']
+    titles = ['Error handling callbacks', 'Error-first argument callbacks',
+              'Empty error handling callbacks', 'Logging error handling callbacks']
 
-    client_metric1_values = get_normalized_rescaled_data(client_array, objects_client, 0, 4)
-    client_metric2_values = get_normalized_rescaled_data(client_array, objects_client, 1, 4)
-    client_metric3_values = get_normalized_rescaled_data(client_array, objects_client, 2, 4)
-    client_metric4_values = get_normalized_rescaled_data(client_array, objects_client, 3, 4)
+    client = normalize_metric(client_array, 0, 4)
+    server = normalize_metric(server_array, 0, 4)
+    print(client)
+    print(server)
 
-    total_lines_client = [get_total_lines_of_code(boot, 4), get_total_lines_of_code(reveal, 4),
-                          get_total_lines_of_code(three, 4), get_total_lines_of_code(brackets, 4),
-                          get_total_lines_of_code(pdf, 4),get_total_lines_of_code(photo, 4)]
+    # [0.0, 0.0, 0.26996612950613386, 0.032814815060342797, 0.036335742258468244, 0.0]
+    # [0.008559445347941454, 0.0, 0.16575597133122072, 0.042341026726692994, 0.13102413226290588, 0.045423574835339542]
 
-    total_metrics_client = [np.sum(get_column_as_array(boot, 2)), np.sum(get_column_as_array(reveal, 2)),
-                            np.sum(get_column_as_array(three, 2)),np.sum(get_column_as_array(brackets, 2)),
-                            np.sum(get_column_as_array(pdf, 2)),np.sum(get_column_as_array(photo, 2))]
+    bar_graph(client, objects_client, '', titles[0])
+    bar_graph(server, objects_server, '', titles[0])
+
+    client = normalize_metric(client_array, 1, 4)
+    server = normalize_metric(server_array, 1, 4)
+    print(client)
+    print(server)
+
+    # [0.2274403737398574, 2.8857784689822017, 0.075390951977072282, 0.39974411073508498, 0.12381067732515107, 0.50385299347954948]
+    # [1.2753573568432766, 0.35174720332141624, 0.3670223027178327, 0.27305641725785684, 1.8367201086309168, 1.1128775834658187]
+
+    bar_graph(client, objects_client, '', titles[1])
+    bar_graph(server, objects_server, '', titles[1])
+
+    client = normalize_metric(client_array, 2, 4)
+    server = normalize_metric(server_array, 2, 4)
+    print(client)
+    print(server)
+
+    # [0.0, 0.0, 0.0, 0.0069607183461333211, 0.0, 0.0]
+    # [0.0, 0.0, 0.00098298574547795847, 0.0017282051725180814, 0.0, 0.0]
+
+    client = normalize_metric(client_array, 3, 4)
+    server = normalize_metric(server_array, 3, 4)
+    print(client)
+    print(server)
+
+    # [0.0061470371281042539, 0.017280110592707794, 0.0083151785268829725, 0.036792368400990413, 0.0053830729271804809, 0.059276822762299938]
+    # [0.64195840109560898, 0.02306539038173221, 0.064016946674252034, 0.085546156039645022, 0.02144031255211187, 0.31796502384737679]
+
+
+    # client_metric1_values = get_normalized_rescaled_data(client_array, objects_client, 0, 4)
+    # client_metric2_values = get_normalized_rescaled_data(client_array, objects_client, 1, 4)
+    # client_metric3_values = get_normalized_rescaled_data(client_array, objects_client, 2, 4)
+    # client_metric4_values = get_normalized_rescaled_data(client_array, objects_client, 3, 4)
     #
-    client_metric_values = [client_metric1_values, client_metric2_values, client_metric3_values, client_metric4_values]
+    # total_lines_client = [get_total_lines_of_code(boot, 4), get_total_lines_of_code(reveal, 4),
+    #                       get_total_lines_of_code(three, 4), get_total_lines_of_code(brackets, 4),
+    #                       get_total_lines_of_code(pdf, 4),get_total_lines_of_code(photo, 4)]
     #
-    # print(total_lines_client)
+    # total_metrics_client = [np.sum(get_column_as_array(boot, 2)), np.sum(get_column_as_array(reveal, 2)),
+    #                         np.sum(get_column_as_array(three, 2)), np.sum(get_column_as_array(brackets, 2)),
+    #                         np.sum(get_column_as_array(pdf, 2)), np.sum(get_column_as_array(photo, 2))]
+    # #
+    # client_metric_values = [client_metric1_values, client_metric2_values, client_metric3_values, client_metric4_values]
+    # #
+    # # print(total_lines_client)
+    # #
+    # server_metric1_values = get_normalized_rescaled_data(server_array, objects_server, 0, 4)
+    # server_metric2_values = get_normalized_rescaled_data(server_array, objects_server, 1, 4)
+    # server_metric3_values = get_normalized_rescaled_data(server_array, objects_server, 2, 4)
+    # server_metric4_values = get_normalized_rescaled_data(server_array, objects_server, 3, 4)
+    # #
+    # total_lines_server = [get_total_lines_of_code(express, 4), get_total_lines_of_code(hexo, 4),
+    #                       get_total_lines_of_code(node, 4), get_total_lines_of_code(node_inspector, 4),
+    #                       get_total_lines_of_code(parse_server, 4), get_total_lines_of_code(socket, 4)]
     #
-    server_metric1_values = get_normalized_rescaled_data(server_array, objects_server, 0, 4)
-    server_metric2_values = get_normalized_rescaled_data(server_array, objects_server, 1, 4)
-    server_metric3_values = get_normalized_rescaled_data(server_array, objects_server, 2, 4)
-    server_metric4_values = get_normalized_rescaled_data(server_array, objects_server, 3, 4)
+    # total_metrics_server = [np.sum(get_column_as_array(express, 2)), np.sum(get_column_as_array(hexo, 2)),
+    #                         np.sum(get_column_as_array(node, 2)), np.sum(get_column_as_array(node_inspector, 2)),
+    #                         np.sum(get_column_as_array(parse_server, 2)), np.sum(get_column_as_array(socket, 2))]
     #
-    total_lines_server = [get_total_lines_of_code(express, 4), get_total_lines_of_code(hexo, 4),
-                          get_total_lines_of_code(node, 4), get_total_lines_of_code(node_inspector, 4),
-                          get_total_lines_of_code(parse_server, 4), get_total_lines_of_code(socket, 4)]
-
-    total_metrics_server = [np.sum(get_column_as_array(express, 2)), np.sum(get_column_as_array(hexo, 2)),
-                            np.sum(get_column_as_array(node, 2)), np.sum(get_column_as_array(node_inspector, 2)),
-                            np.sum(get_column_as_array(parse_server, 2)), np.sum(get_column_as_array(socket, 2))]
-
-    server_metric_values = [server_metric1_values, server_metric2_values, server_metric3_values, server_metric4_values]
+    # server_metric_values = [server_metric1_values, server_metric2_values, server_metric3_values, server_metric4_values]
 
 
     # print(total_lines_server)
