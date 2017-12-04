226d5ee8955608cff39f85fad3e79d326678e0a9
Luana Martins <lms7@cin.ufpe.br>
2017-12-04 00:39:00 -0300

    Remove code from main2

--- statistics/src/index.py
+++ statistics/src/index.py
@@ -29,81 +29,98 @@ def main():
 
     loc_index = 0
 
-    for metric_index in range(2, 49):
+    for metric_index in range(2, 50):
 
-        client_normalized = normalize_metric_by_script(client_matrices, metric_index, loc_index, factor)
-        server_normalized = normalize_metric_by_script(server_matrices, metric_index, loc_index, factor)
+        # client_normalized = normalize_metric_by_script(client_matrices, metric_index, loc_index, factor)
+        # server_normalized = normalize_metric_by_script(server_matrices, metric_index, loc_index, factor)
+        # print('------------------------------------------------------------------')
+        # print('Metric index:', metric_index)
+        # print('Metric name: ', metrics_labels[metric_index])
+        # print('Sample size (client): ', len(client_normalized))
+        # print('Sample size (server): ', len(server_normalized))
+        # print('Median client: ', np.median(client_normalized))
+        # print('Median server: ', np.median(server_normalized))
+        # print('Client: ', client_normalized)
+        # print('Server: ', server_normalized)
+        # try:
+        #     test_result = execute_test(client_normalized, server_normalized)
+        #     print(test_result)
+        #     print(test_result.pvalue)
+        #     # plot_two_groups_histogram_test(client_normalized, server_normalized, metrics_labels[metric_index])
+        # except Exception as inst:
+        #     print(inst)
         print('------------------------------------------------------------------')
-        print('Metric index:', metric_index)
-        print('Metric name: ', metrics_labels[metric_index])
-        print('Sample size (client): ', len(client_normalized))
-        print('Sample size (server): ', len(server_normalized))
-        print('Median client: ', np.median(client_normalized))
-        print('Median server: ', np.median(server_normalized))
-        print('Client: ', client_normalized)
-        print('Server: ', server_normalized)
-        try:
-            print(execute_test(client_normalized, server_normalized))
-        except Exception as inst:
-            print(inst)
-        print('------------------------------------------------------------------')
 
 
-    # bar_line_graph('Empty error handling callbacks', objects_client, total_lines_client, total_metrics_client)
-    # bar_line_graph('Empty error handling callbacks', objects_server, total_lines_server, total_metrics_server)
-    # empty = []
-    # consoleOnly = []
-    # for client in client_array:
-    #     empty.append(percentage(client, 2))
-    #     consoleOnly.append(percentage(client, 3))
-    # print(empty)
-    # print(consoleOnly)
-    # bar_graph(empty, objects_client, 'Percentage', 'Empty callbacks (client-side)')
-    # bar_graph(consoleOnly, objects_client, 'Percentage', 'Logging error (client-side)')
-    #
-    # empty = []
-    # consoleOnly = []
-    # for server in server_array:
-    #     empty.append(percentage(server, 2))
-    #     consoleOnly.append(percentage(server, 3))
-    # print(empty)
-    # print(consoleOnly)
-    #
-    # bar_graph(empty, objects_server, 'Percentage', 'Empty callbacks (server-side)')
-    # bar_graph(consoleOnly, objects_server, 'Percentage', 'Logging error (server-side)')
+    error_handling_metrics_indices = [8, 21, 33, 49]
 
-    # server_metric1_values = get_randomly_n_items(server_metric1_values, len(client_metric1_values))
-    # np.savetxt("server_metric1_values.csv", server_metric1_values, delimiter=",")
-    #
-    # server_metric2_values = get_randomly_n_items(server_metric2_values, len(client_metric2_values))
-    # np.savetxt("server_metric2_values.csv", server_metric2_values, delimiter=",")
-    #
-    # server_metric3_values = get_randomly_n_items(server_metric3_values, len(client_metric3_values))
-    # np.savetxt("server_metric3_values.csv", server_metric1_values, delimiter=",")
-    #
-    # server_metric4_values = get_randomly_n_items(server_metric4_values, len(client_metric4_values))
-    # np.savetxt("server_metric4_values.csv", server_metric4_values, delimiter=",")
+    client_error_handling = 0
+    for matrix in client_matrices:
+        print(error_handling_porcentage_per_matrix(matrix, error_handling_metrics_indices))
+
+    server_error_handling = 0
+    for matrix in server_matrices:
+        print(error_handling_porcentage_per_matrix(matrix, error_handling_metrics_indices))
 
 
+    print(client_error_handling)
+    print(server_error_handling)
+
+        # bar_line_graph('Empty error handling callbacks', objects_client, total_lines_client, total_metrics_client)
+        # bar_line_graph('Empty error handling callbacks', objects_server, total_lines_server, total_metrics_server)
+        # empty = []
+        # consoleOnly = []
+        # for client in client_array:
+        #     empty.append(percentage(client, 2))
+        #     consoleOnly.append(percentage(client, 3))
+        # print(empty)
+        # print(consoleOnly)
+        # bar_graph(empty, objects_client, 'Percentage', 'Empty callbacks (client-side)')
+        # bar_graph(consoleOnly, objects_client, 'Percentage', 'Logging error (client-side)')
+        #
+        # empty = []
+        # consoleOnly = []
+        # for server in server_array:
+        #     empty.append(percentage(server, 2))
+        #     consoleOnly.append(percentage(server, 3))
+        # print(empty)
+        # print(consoleOnly)
+        #
+        # bar_graph(empty, objects_server, 'Percentage', 'Empty callbacks (server-side)')
+        # bar_graph(consoleOnly, objects_server, 'Percentage', 'Logging error (server-side)')
+
+        # server_metric1_values = get_randomly_n_items(server_metric1_values, len(client_metric1_values))
+        # np.savetxt("server_metric1_values.csv", server_metric1_values, delimiter=",")
+        #
+        # server_metric2_values = get_randomly_n_items(server_metric2_values, len(client_metric2_values))
+        # np.savetxt("server_metric2_values.csv", server_metric2_values, delimiter=",")
+        #
+        # server_metric3_values = get_randomly_n_items(server_metric3_values, len(client_metric3_values))
+        # np.savetxt("server_metric3_values.csv", server_metric1_values, delimiter=",")
+        #
+        # server_metric4_values = get_randomly_n_items(server_metric4_values, len(client_metric4_values))
+        # np.savetxt("server_metric4_values.csv", server_metric4_values, delimiter=",")
 
 
-    # plot_two_groups_histogram(client_metric4_values, 'client', server_metric4_values, 'server',
-    #                            'Number of logging callbacks')
 
-    #plot_two_groups_histogram(client_metric1_values, 'client', server_metric1_values, 'server', titles[0], titles[0] + '.png')
-    # plot_two_groups_histogram(client_metric2_values, 'client', server_metric2_values, 'server', titles[1], titles[1] + '.png')
-    # plot_two_groups_histogram(client_metric3_values, 'client', server_metric3_values, 'server', titles[2], titles[2] + '.png')
-    # plot_two_groups_histogram(client_metric4_values, 'client', server_metric4_values, 'server',titles[3], titles[3] + '.png')
-    #
-    #
-    # plot_violinplot([client_metric1_values, server_metric1_values], ['Client', 'Server'], titles[0], titles[0] + '.png')
-    # plot_violinplot([client_metric2_values, server_metric2_values], ['Client', 'Server'], titles[1], titles[1] + '.png')
-    # plot_violinplot([client_metric3_values, server_metric3_values], ['Client', 'Server'], titles[2], titles[2] + '.png')
-    # plot_violinplot([client_metric4_values, server_metric4_values], ['Client', 'Server'], titles[3], titles[3] + '.png')
 
-    # calculate_test(titles, client_metric_values, server_metric_values)
-    # summary(client_metric1_values)
-    # summary(server_metric1_values)
+        # plot_two_groups_histogram(client_metric4_values, 'client', server_metric4_values, 'server',
+        #                            'Number of logging callbacks')
+
+        #plot_two_groups_histogram(client_metric1_values, 'client', server_metric1_values, 'server', titles[0], titles[0] + '.png')
+        # plot_two_groups_histogram(client_metric2_values, 'client', server_metric2_values, 'server', titles[1], titles[1] + '.png')
+        # plot_two_groups_histogram(client_metric3_values, 'client', server_metric3_values, 'server', titles[2], titles[2] + '.png')
+        # plot_two_groups_histogram(client_metric4_values, 'client', server_metric4_values, 'server',titles[3], titles[3] + '.png')
+        #
+        #
+        # plot_violinplot([client_metric1_values, server_metric1_values], ['Client', 'Server'], titles[0], titles[0] + '.png')
+        # plot_violinplot([client_metric2_values, server_metric2_values], ['Client', 'Server'], titles[1], titles[1] + '.png')
+        # plot_violinplot([client_metric3_values, server_metric3_values], ['Client', 'Server'], titles[2], titles[2] + '.png')
+        # plot_violinplot([client_metric4_values, server_metric4_values], ['Client', 'Server'], titles[3], titles[3] + '.png')
+
+        # calculate_test(titles, client_metric_values, server_metric_values)
+        # summary(client_metric1_values)
+        # summary(server_metric1_values)
 
 main()
 
