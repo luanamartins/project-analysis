2d408d28feb2fc14f758c38954ad75922c362818
Luana Martins <lms7@cin.ufpe.br>
2017-12-12 08:05:55 -0300

    Update function naming

--- test/data/event/event4.js
+++ test/data/event/event4.js
@@ -1,5 +1,5 @@
 const io = require("socket.io")(http);
 
 io.on("reason", connection => {
-    socket.on("reason", data => socket.broadcast.emit("reason_failure", data));
+    socket.on("reason", data => socket.broadcast.emit("reason_failure", handleReasonFailure));
 });
