730cf3593cbefd6a4a069e64de780a0b1d65dce9
Luana Martins <lms7@cin.ufpe.br>
2018-04-08 11:04:04 -0300

    Remove the now ignored directory "repos2"

--- retrieve-scripts/repos2/teste/test.js
+++
@@ -1,9 +0,0 @@
-const io = require("socket.io")(http);
-
-function onConnection(socket) {
-    socket.on("drawing_err", function(data) {
-        return socket.broadcast.emit("drawing", data);
-    });
-}
-
-io.on("error_connection", onConnection);
