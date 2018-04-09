46cd1c29231f95d1f1a828f4e06f1b7dac7b0e51
Luana Martins <lms7@cin.ufpe.br>
2018-04-09 18:50:15 -0300

    Add semicolon to the end of statements

--- retrieve-stats-data/index.js
+++ retrieve-stats-data/index.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
-const request = require('request')
-const filesModule = require('./files')
-const fs = require('fs')
+require('dotenv').config();
+const request = require('request');
+const filesModule = require('./files');
+const fs = require('fs');
 const utils = require('./utils.js');
 
 function getAllRepositories(filepath) {
-    const projectPath = process.env.PROJECT_PATH
-    const path = projectPath + '/' + filepath
-    var repositories = fs.readFileSync(path).toString().split('\n')
-    repositories = repositories.map((repository) => repository.replace('https://github.com/', ''))
-    console.log('Got repositories')
+    const projectPath = process.env.PROJECT_PATH;
+    const path = projectPath + '/' + filepath;
+    var repositories = fs.readFileSync(path).toString().split('\n');
+    repositories = repositories.map((repository) => repository.replace('https://github.com/', ''));
+    console.log('Got repositories');
     return repositories
 }
 
 function getRepoDataRequest(repository) {
-    const url = 'http://api.github.com/repos/' + repository
+    const url = 'http://api.github.com/repos/' + repository;
     return getRequest(url)
 }
 
 function getClosedIssuesRequest(repository) {
-    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:issues+state:closed'
+    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:issues+state:closed';
     return getRequest(url)
 }
 
 function getOpenPullRequests(repository) {
-    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:pr+state:open'
+    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:pr+state:open';
     return getRequest(url)
 }
 
 function getClosedPullRequests(repository) {
-    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:pr+state:closed'
+    const url = 'http://api.github.com/search/issues?q=repo:' + repository + '+type:pr+state:closed';
     return getRequest(url)
 }
 
@@ -59,13 +59,13 @@ function getGithubData(repo) {
         getClosedIssuesRequest(repo),
         getOpenPullRequests(repo),
         getClosedPullRequests(repo)
-    ]
+    ];
 
     return Promise.all(promises).then((responses) => {
-        var generalResponse = JSON.parse(responses[0])
-        var closedIssuesResponse = JSON.parse(responses[1])
-        var openPullResponse = JSON.parse(responses[2])
-        var closedPullResponse = JSON.parse(responses[3])
+        var generalResponse = JSON.parse(responses[0]);
+        var closedIssuesResponse = JSON.parse(responses[1]);
+        var openPullResponse = JSON.parse(responses[2]);
+        var closedPullResponse = JSON.parse(responses[3]);
 
         return new Promise(function (resolve, reject) {
             try {
@@ -87,23 +87,23 @@ function getGithubData(repo) {
 }
 
 function deleteFromArray(array, deleteValue) {
-    const result = []
+    const result = [];
     array.forEach(function (item) {
         if (item != deleteValue) {
             result.push(item)
         }
-    })
+    });
 
     return result
 }
 
 
-const projectPath = process.env.PROJECT_PATH
+const projectPath = process.env.PROJECT_PATH;
 
-const repositories = getAllRepositories('data/teste.txt')
-console.log(repositories)
+const repositories = getAllRepositories('data/teste.txt');
+console.log(repositories);
 
-let prom = []
+let prom = [];
 repositories.forEach(function (repo) {
     prom.push(new Promise(function (resolve, reject) {
         getGithubData(repo)
@@ -111,22 +111,22 @@ repositories.forEach(function (repo) {
                 resolve(data)
             })
             .catch(function (err) {
-                resolve({})
+                resolve({});
                 console.log('https://github.com/' + repo)
             })
     }))
-})
+});
 
 Promise.all(prom)
     .then(function (values) {
         //console.log('values ', values)
-        const filename = 'retrieve-stats-data/results.csv'
+        const filename = 'retrieve-stats-data/results.csv';
         const headers = ['repo_name', 'forks', 'stars', 'watchers', 'open_issues',
-            'closed_issues', 'open_pull_requests', 'closed_pull_requests']
+            'closed_issues', 'open_pull_requests', 'closed_pull_requests'];
 
         filesModule.writeCsvFile(filename, headers, deleteFromArray(values, {}))
     })
-    .catch(console.log)
+    .catch(console.log);
 
 
 // const filename = 'retrieve-stats-data/results.csv'
