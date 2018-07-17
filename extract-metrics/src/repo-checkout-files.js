a31902d19d9380fca31cb3082256c4882afe78af
Luana Martins <lms7@cin.ufpe.br>
2018-07-16 21:16:44 -0300

    Update checkout files

--- extract-metrics/src/repo-checkout-files.js
+++ extract-metrics/src/repo-checkout-files.js
@@ -5,21 +5,28 @@ const clone = require('git-clone');
 const path = require('path');
 
 function main() {
-    checkout('client');
+    // checkout('client');
     checkout('server');
 }
 
 function checkout(type) {
     const filepath = CONFIG['dataProjectPath'] + type + '.txt';
     const reposUrl = repoModule.getRepos(filepath);
-    const directory = CONFIG['dataProjectPath'] + 'repo/' + type;
+    const directory = CONFIG['dataProjectPath'] + 'repo2/' + type;
+
+    console.log(directory);
 
     let repoName;
-    let filedir;
     reposUrl.forEach(function (repo) {
         repoName = repo.substring(repo.lastIndexOf('/'));
-        filedir = path.join(directory, repoName);
-        clone(repo, filedir, null, () => { console.log('Checkout: ', filedir) });
+
+        clone(repo, path.join(directory, repoName), null, (err) => {
+            if (err) {
+                console.log('error: ', err)
+            } else {
+                console.log('Checkout: ', repo);
+            }
+        });
     });
 }
 
